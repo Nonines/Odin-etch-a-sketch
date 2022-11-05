70006039041ce084bd4202450ffc5867c6c70e6c
Banky <kingmorty@protonmail.com>
2022-11-05 15:26:45 +0100

    Add option to collect box dimension from user

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
-const container = document.querySelector(".container");
-container.style.cssText = "display: grid; grid-template-columns: repeat(16, 1fr); grid-template-rows: auto;";
-
-for (i = 1; i <= 16*16; i++) {
-    const grid_div = document.createElement("div");
-    grid_div.classList.add("etchBox");
-    grid_div.id = `box${i}`
-    container.appendChild(grid_div);
-}
-
-const etchBox = document.querySelectorAll(".etchBox");
-for (let box of etchBox) {
-    box.addEventListener("mouseleave", () => changeColor(box.id));
+function getUserNumber() {
+    let squaresNum;
+    while (true) {
+        squaresNum = Number(prompt("How many squares would you like your Etch-box to have on each side?"));
+        if (squaresNum <= 100 && squaresNum >= 1) {
+            break;
+        }
+    }
+    return squaresNum;
 }
 
 function changeColor(element) {
@@ -21,3 +17,31 @@ function changeColor(element) {
 
     currentBox.style.backgroundColor = randomColor;
 }
+
+function getEtchSquares() {
+    container.replaceChildren();
+    createEtchBox(getUserNumber());
+}
+
+function createEtchBox(dimension = 16) {
+    container.style.cssText = `display: grid; grid-template-columns: repeat(${dimension}, 1fr); grid-template-rows: auto;`;
+
+    for (i = 1; i <= dimension ** 2; i++) {
+        const grid_div = document.createElement("div");
+        grid_div.classList.add("etchBox");
+        grid_div.id = `box${i}`
+        container.appendChild(grid_div);
+    }
+
+    const etchBox = document.querySelectorAll(".etchBox");
+    for (let box of etchBox) {
+        box.addEventListener("mouseleave", () => changeColor(box.id));
+    }
+}
+
+const button = document.querySelector("button");
+button.addEventListener("click", getEtchSquares);
+
+const container = document.querySelector(".container");
+
+createEtchBox();
