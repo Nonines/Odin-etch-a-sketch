3c6b7eb5b214faa46ef38be4e383a93c719f1082
Banky <kingmorty@protonmail.com>
2022-11-05 13:12:08 +0100

    Add color change on hover to 'etch' boxes

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 const container = document.querySelector(".container");
+container.style.cssText = "display: grid; grid-template-columns: repeat(16, 1fr); grid-template-rows: auto;";
 
 for (i = 1; i <= 16*16; i++) {
     const grid_div = document.createElement("div");
-    // grid_div.textContent = i;
-    grid_div.classList.add("box");
+    grid_div.classList.add("etchBox");
+    grid_div.id = `box${i}`
     container.appendChild(grid_div);
+}
+
+const etchBox = document.querySelectorAll(".etchBox");
+for (let box of etchBox) {
+    box.addEventListener("mouseleave", () => changeColor(box.id));
+}
+
+function changeColor(element) {
+    const currentBox = document.getElementById(element);
+
+    const colors = ["red", "blue", "pink", "green", "yellow"];
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+
+    currentBox.style.backgroundColor = randomColor;
 }
